cdfd19b755869bb4f70e6f65aa6fbb90683f46dc
samprdxn <sameers@axioned.com>
2021-12-22 22:46:13 +0530

    configure environment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require('dotenv').config()
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`App listening at => http://localhost:${port}`);
