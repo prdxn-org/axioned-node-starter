28c9c927471b40853218972746df4c25bd40948d
smayuraxioned <mayurs@prdxn.com>
2022-02-21 14:47:09 +0530

    add swagger docs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   process.stdout.write(`App listening at => http://localhost:${port} \n`);
