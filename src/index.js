c8e4ac04b3c6ce0b70971dcfa5cf762cfc30c2d6
smayuraxioned <mayurs@prdxn.com>
2022-03-02 13:08:39 +0530

    update the swagger config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../swagger.json';
 import Routes from './routes/routes';
 
 dotenv.config();
@@ -9,7 +9,32 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+    description: 'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+    license: {
+      name: 'Licensed Under MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+  },
+  servers: [{
+    url: 'http://localhost:3001',
+    description: 'Development server',
+  }],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/v1', Routes);
 
 app.listen(port, () => {
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,8 +1,38 @@
 import { Router } from 'express';
-import { GetUserData } from '../controller/UserController';
+import { GetUserData, updateUserData } from '../controller/UserController';
 
 const UserRoutes = Router();
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     description: Retrieve a list of users.
+ *     responses:
+ *       200:
+ *         description: Success response.
+ *       400:
+ *         description: This error is sent when invalid details provided.
+ *       500:
+ *         description: Internal server error.
+ */
 UserRoutes.get('/', GetUserData);
+
+/**
+ * @swagger
+ * /update:
+ *   get:
+ *     summary: update a user.
+ *     description: update a users. Can be used to update a user when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: Success response.
+ *       400:
+ *         description: This error is sent when invalid details provided.
+ *       500:
+ *         description: Internal server error.
+ */
+UserRoutes.patch('/update', updateUserData);
 
 export default UserRoutes;
